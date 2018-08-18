45af2960317750211f8c7b56ee351cb4d3c25053
Anthony <starpa@gmail.com>
2018-08-17 22:27:32 -0400

    up to date August 2018

--- rclone_out.sh
+++ rclone_out.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-MONTH=$(date +"%m")
-YEAR=$(date +"%Y")
 DIR=/root/rclone_log.txt
 
-if [ "$(date +%d)" -gt 107 ]; then
+if [ "$(date +%d)" -gt 7 ]; then
 	exit
 else
 	rclone -v copy /mnt/external/NAS/NAS/ remote:clone-backup>>$DIR
-	printf "Completed on: ">>$DIR
-	date>>$DIR
-	printf "\nFiles on Backblaze: ">>$DIR
-	rclone ls remote:clone-backup/Public/daslizmeister/${YEAR}_${MONTH} | wc -l>>$DIR
-	printf "\nFiles on External: ">>$DIR
-	ls /mnt/external/NAS/NAS/Public/daslizmeister/${YEAR}_${MONTH} | wc -l>>$DIR
-	printf "\nFiles on NAS: ">>$DIR
-
-	df -h | grep /mnt/NAS >/dev/null
 	if [ $? -eq 0 ]
 	then
-		ls /mnt/NAS/Public/daslizmeister/${YEAR}_${MONTH} | wc -l>>$DIR
+		printf "Completed on: ">>$DIR
+		date>>$DIR
 	else 
-		printf "NOT ATTACHED">>$DIR
+		printf "FAILED!!!">>$DIR
 	fi
 printf "\n\n#########################\n\n">>$DIR
 fi
--- total-backup-check.sh
+++ total-backup-check.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+i=0
 
 printf "\nFILES ON NAS:\n"
 
-df -h | grep /mnt/NAS>/dev/null
-if [ $? -eq 0 ]
-then
-	cd /mnt/NAS
-	find -maxdepth 1 -type d | while read -r dir; do printf "%s:\t" "$dir"; find "$dir" -type f | wc -l; done
+while [ $i -lt 3 ]
+do
 
+df -h | grep /mnt/NAS>/dev/null
+
+if [ $? -eq 0 ]
+	then
+	break
 else
-	printf "NOT ATTACHED"
+	printf "Mounting...\n"
+	mount -a
+	sleep 4
 fi
+i=$((i+1))
+done
+
+cd /mnt/NAS
+find -maxdepth 1 -type d | while read -r dir; do printf "%s:\t" "$dir"; find "$dir" -type f | wc -l; done
+
 
 printf "\n\nFILES ON EXTERNAL:\n"
 
@@ -20,3 +31,4 @@ find -maxdepth 1 -type d | while read -r dir; do printf "%s:\t" "$dir"; find "$d
 
 printf "\n\nFILES ON CLOUD:\n"
 rclone size remote:clone-backup
+printf "\n\n"
